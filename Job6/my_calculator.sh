69fed9da58d743ad4f3b73ce600fb85f78dbedf5
Clément <113895445+Marin-Clement@users.noreply.github.com>
2022-10-13 14:31:40 +0200

    Made a calculator

---
+++ Job6/my_calculator.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+if [ $2 = "+" ] || [ $2 = "-" ] || [ $2 = "/" ] || [ $2 = "x" ]
+
+then
+
+        if [ $2 = "+" ]
+
+        then echo "scale=4; $1 + $3" | bc
+
+        elif [ $2 = "-" ]
+
+        then echo "scale=4; $1 - $3" | bc
+
+        elif [ $2 = "/" ]
+
+        then echo "scale=4; $1 / $3" | bc
+
+        elif [ $2 = "x" ]
+
+        then echo "scale=4; $1 * $3" | bc
+
+        fi
+
+else echo "Quoiiii ??"
+fi
